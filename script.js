e8ff7dab8424e40e1e2c517285ba9bb1253eb858
Gibholk <novitnijmatvej@gmal.com>
2024-12-27 18:47:33 +0300

    New commit

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector('.searchBarContainer').style.backgroundColor = '#f5f5f5';
+
+const notification = document.querySelector('.notification');
+if (notification) {
+  notification.style.display = 'flex';
+  notification.style.width = '100%';
+  notification.style.flexDirection = 'column';
+}
